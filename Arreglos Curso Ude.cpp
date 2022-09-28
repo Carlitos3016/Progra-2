bff646e90b5c491f0b35fbcf554346b6dece0df8
Carlitos3016 <114548555+Carlitos3016@users.noreply.github.com>
2022-09-27 21:09:01 -0600

    Create Arreglos Curso Ude.cpp

---
+++ Arreglos Curso Ude.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <cmath>
+using namespace std; 
+
+int main(){
+
+  string nombre[]= { "Carlos", "Alberto", "Dominguez", "Garcia"};
+ 
+ cout << nombre[2];
+
+
+
+
+
+
+return 0;
+}
+
